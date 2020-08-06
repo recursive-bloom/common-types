da9da5fb9f97b2a6fd2b41b7cfdb557d4f135972
lvqinghao <lvqinghao@163.com>
2020-08-06 21:16:43 +0800

    add ipc.rs

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,4 +1,5 @@
 
+use hex_literal::hex;
 use rlp::{Decodable, DecoderError, Encodable, Rlp, RlpStream};
 
 /// RLP-Encode( method(string), id(number), param(rlp-encoded-list) );
@@ -15,31 +16,12 @@ pub struct IpcReply {
     pub result: Vec<u8>,
 }
 
-impl IpcRequest {
-    pub fn new(method : String, id : u64, params : Vec<u8>) -> Self {
-        IpcRequest{
-            method,
-            id,
-            params
-        }
-    }
-}
-
-impl IpcReply {
-    pub fn new(method : String, id : u64, result : Vec<u8>) -> Self {
-        IpcReply{
-            id,
-            result,
-        }
-    }
-}
-
 impl Encodable for IpcRequest {
     fn rlp_append(&self, s: &mut RlpStream){
         s.begin_list(3);
         s.append(&self.method);
         s.append(&self.id);
-        s.append(&self.params);
+        s.append_list(&self.params);
     }
 }
 
@@ -54,7 +36,7 @@ impl Decodable for IpcRequest {
         Ok(IpcRequest {
             method: rlp.val_at(0)?,
             id: rlp.val_at(1)?,
-            params: rlp.val_at(2)?,
+            params: rlp.list_at(2)?,
         })
     }
 }
@@ -83,8 +65,38 @@ impl Decodable for IpcReply {
 }
 
 #[test]
-fn xx() {
-    println!("hello");
+fn test_req() {
+    use ethereum_types::{H256, H160, Address, U256, BigEndianHash};
+    use super::transaction::{UnverifiedTransaction, Action};
+    use parity_crypto::publickey::{Signature, Secret, Public, recover, public_to_address};
+    use std::str::FromStr;
+
+    let data = hex!("f85f800182520894095e7baea6a6c7c4c2dfeb977efac326af552d870a801ba048b55bfa915ac795c431978d8a6a992b628d557da5ff759b307d495a36649353a0efffd310ac743f371de3b9f7f9cb56c0b28ad43601b4ab949f53faa07bd2c804");
+    //let data = hex!("778899");
+    let ipc_request = IpcRequest {
+        method: "hello".to_string(),
+        id: 123,
+        params: data.to_vec(),
+    };
+
+    let req_bytes = ipc_request.rlp_bytes();
+    println!("ipc_request hex-string: {}", hex::encode(req_bytes));
+    let recovered_request : IpcRequest = rlp::decode(&ipc_request.rlp_bytes()).unwrap();
+    println!("recovered_request: {:x?}", recovered_request);
+
+    let bytes = recovered_request.params;
+
+    let t: UnverifiedTransaction = rlp::decode(&bytes).expect("decoding UnverifiedTransaction failed");
+    assert_eq!(t.data, b"");
+    assert_eq!(t.gas, U256::from(0x5208u64));
+    assert_eq!(t.gas_price, U256::from(0x01u64));
+    assert_eq!(t.nonce, U256::from(0x00u64));
+    if let Action::Call(ref to) = t.action {
+        assert_eq!(*to, Address::from_str("095e7baea6a6c7c4c2dfeb977efac326af552d87").unwrap());
+    } else { panic!(); }
+    assert_eq!(t.value, U256::from(0x0au64));
+    assert_eq!(public_to_address(&t.recover_public().unwrap()), Address::from_str("0f65fe9276bc9a24ae7083ae28e2660ef72df99e").unwrap());
+    assert_eq!(t.chain_id(), None);
 }
 
 
